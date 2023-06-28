047c711ce16ea0d4fc83d187960cea2a0d732e11
0hMyGoat <daftouney@gmail.com>
2023-06-28 13:42:31 +0200

    feat: implémentation cours dans les ressources :sparkles:

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 
 import { Admin, Resource, ListGuesser, EditGuesser, ShowGuesser } from 'react-admin';
 import { dataProvider } from './dataProvider';
+import {CourseList} from "./courses/CourseList";
+import {CourseEdit} from "./courses/CourseEdit";
+import {CourseShow} from "./courses/CourseShow";
 
 export const App = () => (
     <Admin
         dataProvider={dataProvider}
 	>
-        <Resource name="courses" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
+        <Resource name="courses" list={CourseList} edit={CourseEdit} show={CourseShow} create={CourseEdit}/>
 		<Resource name="computers" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
 		<Resource name="loans" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
     </Admin>
